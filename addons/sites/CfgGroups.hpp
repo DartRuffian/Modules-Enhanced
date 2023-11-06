4c4152493473145ccbd14c627e4ec134a2b936d3
Hypoxic <martini_a@outlook.com>
2023-11-06 08:13:53 -0500

    update

---
+++ addons/sites/CfgGroups.hpp
@@ -0,0 +1,35 @@
+class CfgGroups {
+    class MEH_Sites {
+        class VanillaNato {
+            class TeamRifle {
+                units[] = {
+                    "B_Soldier_TL_F",
+                    "B_Soldier_F",
+                    "B_soldier_AR_F",
+                    "B_medic_F"
+                };
+                ranks[] = {
+                    "SERGEANT",
+                    "PRIVATE",
+                    "PRIVATE",
+                    "CORPORAL"
+                };
+            };
+            class TeamAntiTank {
+                units[] = {
+                    "B_Soldier_TL_F",
+                    "B_soldier_AAT_F",
+                    "B_medic_F",
+                    "B_soldier_AT_F",
+                };
+                ranks[] = {
+                    "SERGEANT",
+                    "PRIVATE",
+                    "PRIVATE",
+                    "CORPORAL"
+                };
+            };
+            
+        }
+    }
+}
---
+++ addons/sites/script_component.hpp
@@ -0,0 +1,3 @@
+#define COMPONENT sites
+
+#include "\z\meh\addons\main\script_mod.hpp"
