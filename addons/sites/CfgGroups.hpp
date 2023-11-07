4dafcc5c9978832b8520c7f2c69c7d4eefba0c6f
Hypoxic <martini_a@outlook.com>
2023-11-07 01:33:54 -0500

    transfer computers

--- addons/sites/CfgGroups.hpp
+++ addons/sites/CfgGroups.hpp
@@ -1,33 +1,47 @@
 class MEH_Sites {
     class NATO {
-        class TeamRifle {
-            units[] = {
-                "B_Soldier_TL_F",
-                "B_Soldier_F",
-                "B_soldier_AR_F",
-                "B_medic_F"
-            };
-            ranks[] = {
-                "SERGEANT",
-                "PRIVATE",
-                "PRIVATE",
-                "CORPORAL"
-            };
-        };
-        class TeamAntiTank {
-            units[] = {
-                "B_Soldier_TL_F",
-                "B_soldier_AAT_F",
-                "B_medic_F",
-                "B_soldier_AT_F",
-            };
-            ranks[] = {
-                "SERGEANT",
-                "PRIVATE",
-                "PRIVATE",
-                "CORPORAL"
+        class Roadblock {
+            class Group {
+                class units[] = {
+                    "B_Soldier_TL_F",
+                    "B_soldier_LAT2_F",
+                    "B_soldier_LAT2_F",
+                    "B_HeavyGunner_F"
+                };
+                class ranks[] = {
+                    "SERGEANT",
+                    "PRIVATE",
+                    "PRIVATE",
+                    "CORPORAL"
+                };
+                class vehicles[] = {
+                    "B_MRAP_01_hmg_F"
+                };
             };
         };
-        
-    }
-}
+        class ObservationPost {
+            class Group {
+                class units[] = {
+                    "B_sniper_F",
+                    "B_spotter_F"
+                };
+                class ranks[] = {
+                    "SERGEANT",
+                    "SERGEANT"
+                };
+                class vehicles[] = {
+                    "B_Quadbike_01_F"
+                };
+            };
+        };
+        class RadarSite
+            class Group {
+                class units[] = {
+                    
+                };
+                class vehicles[] = {
+                    "B_Radar_System_01_F"
+                }
+            }
+    };
+};
